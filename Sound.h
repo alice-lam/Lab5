f776f276dac8e9e300bda6db8760537971aa4a4e
aznfrosting <alice94@sbcglobal.net>
2016-09-28 20:12:28 -0500

    More commits

---
+++ Sound.h
@@ -0,0 +1,34 @@
+// Song.h
+// Runs on TM4C123
+#ifndef MUSIC_H
+#define MUSIC_H 1
+
+#include <stdint.h>
+
+// if desired interrupt frequency is f, Timer0A_Init parameter is busfrequency/f
+#define F16HZ (80000000/16)
+#define F20KHZ (80000000/20000)
+#define F64HZ	(80000000/64)
+#define ENVELOPE_SCALE 100000
+
+
+#define Whole 16
+#define Half 8
+#define Quarter 4
+#define Eighth 2
+#define Sixteenth 1
+
+typedef struct {
+	uint16_t pitch;    //frequency for note
+	uint16_t duration; //number of beats for note
+} Note;
+
+typedef struct {
+	uint16_t tempo;    //beats per minute
+	Note *notes;       //all of the notes in the song
+} Song;
+
+void Song_PlayInit(int song);
+void Song_PlayHandler(void);
+
+#endif
