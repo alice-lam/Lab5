fad689863a3d175ce5f333f5c4784ee42e177fff
aznfrosting <alice94@sbcglobal.net>
2016-10-02 14:01:31 -0500

    Added some timer stuff

--- Time.c
+++ Time.c
@@ -18,6 +18,7 @@ void EnableInterrupts(void);  // Enable interrupts
 long StartCritical (void);    // previous I bit, disable interrupts
 void EndCritical(long sr);    // restore I bit to previous value
 void WaitForInterrupt(void);  // low power mode
+void (*PeriodicTask)(void);   // user function
 
 int32_t count;
 int32_t alarmflag; 
@@ -42,6 +43,34 @@ void Timer0A_Init1HzInt(void){
                                    // Timer0A=priority 2
   NVIC_PRI4_R = (NVIC_PRI4_R&0x00FFFFFF)|0x40000000; // top 3 bits
   NVIC_EN0_R = 1<<19;              // enable interrupt 19 in NVIC
+}
+// ***************** Timer0A_Init ****************
+// Activate TIMER0 interrupts to run user task periodically
+// Inputs:  task is a pointer to a user function
+//          period in units (1/clockfreq), 32 bits
+// Outputs: none
+void Timer0A_Init(void(*task)(void), uint32_t period){long sr;
+  sr = StartCritical(); 
+  SYSCTL_RCGCTIMER_R |= 0x01;   // 0) activate TIMER0
+  PeriodicTask = task;          // user function
+  TIMER0_CTL_R = 0x00000000;    // 1) disable TIMER0A during setup
+  TIMER0_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+  TIMER0_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
+  TIMER0_TAILR_R = period-1;    // 4) reload value
+  TIMER0_TAPR_R = 0;            // 5) bus clock resolution
+  TIMER0_ICR_R = 0x00000001;    // 6) clear TIMER0A timeout flag
+  TIMER0_IMR_R = 0x00000001;    // 7) arm timeout interrupt
+  NVIC_PRI4_R = (NVIC_PRI4_R&0x00FFFFFF)|0x80000000; // 8) priority 4
+// interrupts enabled in the main program after all devices initialized
+// vector number 35, interrupt number 19
+  NVIC_EN0_R = 1<<19;           // 9) enable IRQ 19 in NVIC
+  TIMER0_CTL_R = 0x00000001;    // 10) enable TIMER0A
+  EndCritical(sr);
+}
+
+void Timer0A_Handler(void){
+  TIMER0_ICR_R = TIMER_ICR_TATOCINT;// acknowledge timer0A timeout
+  (*PeriodicTask)();                // execute user task
 }
 
 // **************SysTick_Init*********************
@@ -84,19 +113,6 @@ void Timer1_Init(void){
 // vector number 37, interrupt number 21
   NVIC_EN0_R = 1<<21;           // 9) enable IRQ 21 in NVIC
   TIMER1_CTL_R = 0x00000001;    // 10) enable TIMER1A
-}
-
-void Timer0A_Handler(void){
-  TIMER0_ICR_R = TIMER_ICR_TATOCINT;    // acknowledge timer0A timeout
-  PF2 ^= 0x04;                   // profile
-	if (count < 86399){
-		count++;
-	}else{
-	count = 0;
-	}
-	if (count == alarm1){
-		alarmflag = 1;
-	}
 }
 
 int32_t whattime(void){
